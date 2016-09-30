263bb277e60a5c9b6e898e048394657cdada55a6
dragosa <dragosa@dragosa-MacBook-Pro.local>
2016-09-30 17:26:57 +0300

    dd
    dd

---
+++ ebsample/settings.py
@@ -0,0 +1,21 @@
+import environ
+from base_settings import *
+
+current = environ.Path(__file__)
+root = current - 3  # three folder back (/a/b/c/ - 3 = /)
+project_root = current - 2
+
+env = environ.Env(DEBUG=(bool, False),)  # set default values and casting
+
+SECRET_KEY = env('SECRET_KEY')
+
+DEBUG = env('DEBUG')  # False if not in os.environ
+
+DATABASES = {
+    'default': env.db(),
+}
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+STATIC_ROOT = project_root('static')
+STATIC_URL = '/static/'
