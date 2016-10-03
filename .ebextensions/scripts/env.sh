2dafa365e4d28e8974a39a4a88b7540e33669ebf
dragosa83 <dragosa@emind.co>
2016-10-03 12:37:39 +0000

    dad
    dad

--- .ebextensions/scripts/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#setup env variable for ENV_NAME:
-dirCurScript=$(dirname "${BASH_SOURCE[0]}")
-
-export ENV_NAME="testApp-1"
-
--- .ebextensions/scripts/setup/logdir-config.sh
+++
@@ -1,8 +0,0 @@
-TODAY=$(date +%Y%m%d)
-TARGET_DIR="/var/log/nginx"
-
-
-
-logger -t "logdir-config" "Creating logging symlink ..."
-OUTPUT=$(ln -v -s "${TARGET_DIR}" "/var/log/nginx")
-logger -t "logdir-config" "$OUTPUT"
